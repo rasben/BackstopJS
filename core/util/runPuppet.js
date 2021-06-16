53e1e208af6346294ff058620931fd9fe8db80c3
Benjamin Rasmussen <benjamin@reload.dk>
2021-06-16 17:26:13 +0200

    Set networkidle0 as alternative to delays

--- core/util/runPuppet.js
+++ core/util/runPuppet.js
@@ -116,7 +116,7 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
     if (isReference && scenario.referenceUrl) {
       url = scenario.referenceUrl;
     }
-    await page.goto(translateUrl(url));
+    await page.goto(translateUrl(url), {"waitUntil" : "networkidle0"});
 
     await injectBackstopTools(page);
 
